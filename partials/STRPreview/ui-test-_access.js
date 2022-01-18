88794e1bf1fe2a2c41b3855dd1e03ba2c5b2d7e7
Rosano Coutinho <pub@xeari.com>
2022-01-18 07:44:10 -0500

    Spec STRPreview

---
+++ partials/STRPreview/ui-test-_access.js
@@ -0,0 +1,39 @@
+const kDefaultRoutePath = '/';
+
+Object.entries({
+	STRPreview: '.STRPreview',
+	STRPreviewNumber: '.STRPreviewNumber',
+	STRPreviewDate: '.STRPreviewDate',
+	STRPreviewTitle: '.STRPreviewTitle',
+	STRPreviewNotes: '.STRPreviewNotes',
+}).map(function (e) {
+	return global[e.shift()]  = e.pop();
+});
+
+describe('STRPreview_Access', function () {
+
+	before(function() {
+		return browser.visit(kDefaultRoutePath);
+	});
+	
+	it('shows STRPreview', function() {
+		browser.assert.elements(STRPreview, 1);
+	});
+	
+	it('shows STRPreviewNumber', function() {
+		browser.assert.elements(STRPreviewNumber, 1);
+	});
+	
+	it('shows STRPreviewDate', function() {
+		browser.assert.elements(STRPreviewDate, 1);
+	});
+	
+	it('shows STRPreviewTitle', function() {
+		browser.assert.elements(STRPreviewTitle, 1);
+	});
+	
+	it('shows STRPreviewNotes', function() {
+		browser.assert.elements(STRPreviewNotes, 1);
+	});
+	
+});
---
+++ partials/STRPreview/ui-test-_misc.js
@@ -0,0 +1,61 @@
+const kDefaultRoutePath = '/';
+
+describe('STRPreview_Misc', function () {
+
+	before(function () {
+		return browser.visit(kDefaultRoutePath);
+	});
+
+	describe('STRPreview', function test_STRPreview () {
+
+		it('classes OLSKCommonCard', function () {
+			browser.assert.hasClass(STRPreview, 'OLSKCommonCard');
+		});
+	
+	});
+
+	describe('STRPreviewNumber', function test_STRPreviewNumber () {
+
+		it('sets text', function () {
+			browser.assert.text(STRPreviewNumber, '0000');
+		});
+	
+	});
+
+	describe('STRPreviewDate', function test_STRPreviewDate () {
+
+		it('sets datetime', function () {
+			browser.assert.attribute(STRPreviewDate, 'datetime', '2022-01-18');
+		});
+
+		it('sets text', function () {
+			browser.assert.text(STRPreviewDate, 'January 18th, 2022');
+		});
+	
+	});
+
+	describe('STRPreviewTitle', function test_STRPreviewTitle () {
+
+		it('sets href', function () {
+			browser.assert.attribute(STRPreviewTitle, 'href', '/0000/');
+		});
+
+		it('sets text', function () {
+			browser.assert.text(STRPreviewTitle, 'STREpisodeTitleText');
+		});
+	
+	});
+
+	describe('STRPreviewNotes', function test_STRPreviewNotes () {
+
+		it('sets href', function () {
+			browser.assert.attribute(STRPreviewNotes, 'href', '/0000/');
+		});
+
+		it('sets text', function () {
+			browser.assert.text(STRPreviewNotes, '→ Episode notes');
+		});
+	
+	});
+
+});
