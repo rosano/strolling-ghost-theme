faab443fdd3f600fb61d61cfc02f51c342b81ddc
Rosano Coutinho <pub@xeari.com>
2022-01-18 07:59:56 -0500

    Extract STRVitrine

--- partials/STRVitrine/ui-test-_access.js
+++ partials/STRVitrine/ui-test-_access.js
@@ -1,22 +1,23 @@
 const kDefaultRoutePath = '/';
 
 Object.entries({
+	STRVitrine: '.STRVitrine',
 }).map(function (e) {
 	return global[e.shift()]  = e.pop();
 });
 
-describe('STRTheme_Access', function () {
+describe('STRVitrine_Access', function () {
 
 	before(function() {
 		return browser.visit(kDefaultRoutePath);
 	});
 	
 	it('shows STRVitrine', function() {
-		browser.assert.elements('.STRVitrine', 1);
+		browser.assert.elements(STRVitrine, 1);
 	});
 	
 	it('shows STRAbove', function() {
-		browser.assert.elements('.STRAbove', 1);
+		browser.assert.elements(STRAbove, 1);
 	});
 
 });
