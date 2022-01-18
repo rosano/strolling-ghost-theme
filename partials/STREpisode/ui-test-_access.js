fb9462932a1074c67ee9a1ac7eab7a85ff71b51a
Rosano Coutinho <pub@xeari.com>
2022-01-18 07:44:28 -0500

    Spec STREpisode

---
+++ partials/STREpisode/ui-test-_access.js
@@ -0,0 +1,53 @@
+const kDefaultRoutePath = '/0000/';
+
+Object.entries({
+	STREpisode: '.STREpisode',
+	STREpisodeAll: '.STREpisodeAll',
+	STREpisodeMeta: '.STREpisodeMeta',
+	STREpisodeMetaNumber: '.STREpisodeMetaNumber',
+	STREpisodeMetaDate: '.STREpisodeMetaDate',
+	STREpisodeTitle: '.STREpisodeTitle',
+	STREpisodeContent: '.STREpisodeContent',
+}).map(function (e) {
+	return global[e.shift()]  = e.pop();
+});
+
+describe('STREpisode_Access', function () {
+
+	before(function() {
+		return browser.visit(kDefaultRoutePath);
+	});
+	
+	it('shows STREpisode', function() {
+		browser.assert.elements(STREpisode, 1);
+	});
+	
+	it('shows STREpisodeAll', function() {
+		browser.assert.elements(STREpisodeAll, 1);
+	});
+
+	it('shows STREpisodeMeta', function () {
+		browser.assert.elements(STREpisodeMeta, 1);
+	});
+	
+	it('shows STREpisodeMetaNumber', function() {
+		browser.assert.elements(STREpisodeMetaNumber, 1);
+	});
+	
+	it('shows STREpisodeMetaDate', function() {
+		browser.assert.elements(STREpisodeMetaDate, 1);
+	});
+	
+	it('shows STREpisodeTitle', function() {
+		browser.assert.elements(STREpisodeTitle, 1);
+	});
+	
+	it('shows STREpisodeContent', function() {
+		browser.assert.elements(STREpisodeContent, 1);
+	});
+	
+	it('shows STRIntro', function() {
+		browser.assert.elements('.STRIntro', 1);
+	});
+	
+});
---
+++ partials/STREpisode/ui-test-_misc.js
@@ -0,0 +1,61 @@
+const kDefaultRoutePath = '/0000/';
+
+describe('STREpisode_Misc', function () {
+
+	before(function () {
+		return browser.visit(kDefaultRoutePath);
+	});
+
+	describe('STREpisodeAll', function test_STREpisodeAll () {
+
+		it('sets href', function () {
+			browser.assert.attribute(STREpisodeAll, 'href', '/');
+		});
+
+		it('sets text', function () {
+			browser.assert.text(STREpisodeAll, '← All episodes');
+		});
+	
+	});
+
+	describe('STREpisodeMetaNumber', function test_STREpisodeMetaNumber () {
+
+		it('sets text', function () {
+			browser.assert.text(STREpisodeMetaNumber, 'STRTitleText Episode 0000');
+		});
+	
+	});
+
+	describe('STREpisodeMetaDate', function test_STREpisodeMetaDate () {
+
+		it('sets datetime', function () {
+			browser.assert.attribute(STREpisodeMetaDate, 'datetime', '2022-01-18');
+		});
+
+		it('sets text', function () {
+			browser.assert.text(STREpisodeMetaDate, 'January 18th, 2022');
+		});
+	
+	});
+
+	describe('STREpisodeTitle', function test_STREpisodeTitle () {
+
+		it('sets href', function () {
+			browser.assert.attribute(STREpisodeTitle, 'href', '/0000/');
+		});
+
+		it('sets text', function () {
+			browser.assert.text(STREpisodeTitle, 'STREpisodeTitleText');
+		});
+	
+	});
+
+	describe('STREpisodeContent', function test_STREpisodeContent () {
+
+		it('sets text', function () {
+			browser.assert.text(STREpisodeContent, 'STREpisodeContentText');
+		});
+	
+	});
+
+});
