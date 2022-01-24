20ed80e1d4b3cca604e4607608d57f9a0bdb7f83
Rosano Coutinho <pub@xeari.com>
2022-01-24 12:35:41 -0500

    Spec STR_DefaultTwitterPlayerCard

--- partials/STR_Default/ui-test-_misc.js
+++ partials/STR_Default/ui-test-_misc.js
@@ -15,6 +15,30 @@ describe('STR_Default_Misc', function () {
 		it('classes OLSKDecorCapped', function () {
 			browser.assert.hasClass(STR_Default, 'OLSKDecorCapped');
 		});
+
+		context('STR_DefaultTwitterPlayerCard', function () {
+			
+			before(function () {
+				return browser.visit('/0000/');
+			});
+
+			it.skip('sets meta[name="twitter:card"]', function () {
+				browser.assert.attribute('meta[name="twitter:card"]', 'content', 'player');
+			});
+
+			it('sets meta[name="twitter:player"]', function () {
+				browser.assert.attribute('meta[name="twitter:player"]', 'content', 'https://go.rosano.ca/0000/');
+			});
+
+			it('sets meta[name="twitter:player:width"]', function () {
+				browser.assert.attribute('meta[name="twitter:player:width"]', 'content', '500');
+			});
+
+			it('sets meta[name="twitter:player:height"]', function () {
+				browser.assert.attribute('meta[name="twitter:player:height"]', 'content', '180');
+			});
+
+		});
 	
 	});
 
