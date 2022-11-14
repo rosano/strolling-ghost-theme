8a8af654221537b809ad0a8851c1c114a01d458e
Rosano Coutinho <pub@xeari.com>
2022-11-14 12:42:56 +0100

    Spec STRGuest

---
+++ partials/STRGuest/ui-test-_access.js
@@ -0,0 +1,49 @@
+const kDefaultRoutePath = '/guests/';
+
+Object.entries({
+	STRGuest: '.STRGuest',
+	STRGuestHeading: '.STRGuestHeading',
+	STRGuestLink: '.STRGuestLink',
+	STRGuestLink_Example: '.STRGuestLink:nth-of-type(1)',
+	STRGuestLinkName: '.STRGuestLinkName',
+}).map(function (e) {
+	return global[e.shift()] = e.pop();
+});
+
+describe('STRGuest_Access', function () {
+
+	const postCount = 1;
+
+	before(function() {
+		return browser.visit(kDefaultRoutePath);
+	});
+	
+	it('shows STRGuest', function() {
+		browser.assert.elements(STRGuest, postCount);
+	});
+	
+	it('shows STRGuestHeading', function () {
+		browser.assert.elements(STRGuestHeading, 1);
+	});
+
+	it('shows STRGuestLink', function () {
+		browser.assert.elements(STRGuestLink, postCount);
+	});
+
+	it('shows STRGuestLinkName', function () {
+		browser.assert.elements(STRGuestLinkName, 1);
+	});
+
+	it('hides moos', function () {
+		browser.assert.elements('.STRGuestLink[href="/tag/moos/"]', 0);
+	});
+
+	it('hides money', function () {
+		browser.assert.elements('.STRGuestLink[href="/tag/money/"]', 0);
+	});
+
+	it('hides meta', function () {
+		browser.assert.elements('.STRGuestLink[href="/tag/meta/"]', 0);
+	});
+	
+});
---
+++ partials/STRGuest/ui-test-_misc.js
@@ -0,0 +1,41 @@
+const kDefaultRoutePath = '/guests/';
+
+describe('STRGuest_Misc', function () {
+
+	before(function () {
+		return browser.visit(kDefaultRoutePath);
+	});
+
+	describe('STRGuestHeading', function test_STRGuestHeading () {
+
+		it('sets text', function () {
+			browser.assert.text(STRGuestHeading, 'Guests');
+		});
+	
+	});
+
+	describe('STRGuestLink', function test_STRGuestLink () {
+
+		it('sets aria-text', function () {
+			browser.assert.attribute(STRGuestLink_Example, 'aria-text', 'STRTag');
+		});
+	
+		it('sets style', function () {
+			browser.assert.attribute(STRGuestLink_Example, 'style', /\/content\/images.*\/STRTagImage/);
+		});
+
+		it('sets href', function () {
+			browser.assert.attribute(STRGuestLink_Example, 'href', '/tag/strtag/');
+		});
+
+	});
+
+	describe('STRGuestLinkName', function test_STRGuestLinkName () {
+
+		it('sets text', function () {
+			browser.assert.text(STRGuestLink, 'STRTag');
+		});
+	
+	});
+
+});
